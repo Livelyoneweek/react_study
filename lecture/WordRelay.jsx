fdc80162ef0d07ebf48106897b7edabed4878cd7
Livelyoneweek <wlvvwhs@naver.com>
2023-06-16 00:28:56 +0900

    to hooks

--- lecture/WordRelay.jsx
+++ lecture/WordRelay.jsx
@@ -1,52 +1,54 @@
 const React = require('react');
-const { Component } = React;
+const { useState, useRef } = React;
 
-class WordRelay extends Component {
-    state = {
-        word: '제로초',
-        value: '',
-        result: '',
-    };
 
-    onSubmitForm = (e) => {
+// const { Component } = React;
+// class WordRelay extends Component {
+    // state = {
+    //     word: '제로초',
+    //     value: '',
+    //     result: '',
+    // };
+
+const WordRelay = () => {
+    const [word, setword] = useState('제로초');
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
+    const inputRef = useRef(null);
+
+    const onSubmitForm = (e) => {
         e.preventDefault();
-        if(this.state.word[this.state.word.length -1] === this.state.value[0]) {
-            this.setState({
-                result:'딩동댕',
-                word: this.state.value,
-                value: '',
-            });
-            this.input.focus();
+        if(word[word.length -1] === value[0]) {
+            setResult('딩동댕');
+            setword(value);
+            setValue('');
+            inputRef.current.focus();
         } else {
-            this.setState({
-                result: '땡',
-                value: '',
-            })
-            this.input.focus();
+            setResult('땡');
+            setValue('');
+            inputRef.current.focus();
         }
     }
 
-    onChangeInput = (e) => {
-        this.setState({ value:e.currentTarget.value});
+    const onChangeInput = (e) => {
+        setValue(e.currentTarget.value);
     }
 
-    input;
-    onRefInput = (c) => {
-        this.input = c;
-    }
+
     //input에 value와 onChange는 세트이다, 아니면 defaultValue 사용
-    render() {
-        return (
-        <>
-            <div>{this.state.word}</div>
-            <form onSubmit={this.onSubmitForm}>
-                <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
-                <button>입력!!</button>
-            </form>
-            <div>{this.state.result}</div>
-        </>
-        )
-    }
+
+    return (
+    <>
+        <div>{word}</div>
+        <form onSubmit={onSubmitForm}>
+            <label htmlFor="wordInput">글자를 입력하세요</label>
+            <input id="wordInput" className='wordInput' ref={inputRef} value={value} onChange={onChangeInput} />
+            <button>입력!!</button>
+        </form>
+        <div>{result}</div>
+    </>
+    )
+    
 }
 
 
