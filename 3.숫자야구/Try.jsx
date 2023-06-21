cf1cc0f25439dfb815da8bbd09f49d6ca736785f
Livelyoneweek <wlvvwhs@naver.com>
2023-06-22 00:31:08 +0900

    memo 주석 추가

--- 3.숫자야구/Try.jsx
+++ 3.숫자야구/Try.jsx
@@ -1,5 +1,6 @@
 import React, { memo } from 'react';
 
+// memo는 부모컴포터는가 리 렌더링 됬을 떄 자식 컴포턴트가 리 렌더링 되는 것을 막아줌
 const Try = memo( ({tryInfo}) => {
   return (
     <li>
@@ -8,6 +9,7 @@ const Try = memo( ({tryInfo}) => {
     </li>
   );
 });
+// memo 쓸떄 displayName도 할것
 Try.displayName = 'Try';
 
 export default Try;
